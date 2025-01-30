329339eea10f921663a1e504d62311c13e73809a
Doriann06 <doriann.bak@gmail.com>
2025-01-30 09:18:33 +0100

    Add files via upload

---
+++ js/calcul.js
@@ -0,0 +1,66 @@
+// Ce script va permettre d'écrire dans les champs nombreA et nombreB
+// et lire la réponse de l'utilisateur, lorsqu'il cliquera le bouton.
+
+function tirageNombre(min,max){
+    // cette fonction tire un nombre au hasard entre min et max
+    let nombre=Math.floor(Math.random()*(max-min+1)+min)
+        return nombre
+}
+function ecrireNombre(ID,valeur){
+    document.querySelector(ID).value=valeur
+    // document.getElementById(ID).value = valeur // autre possibilité
+
+}
+function lireNombre(ID){
+    return Number (document.querySelector(ID).value)
+}
+
+for (let t=0; t < 50; t++) {
+    console.log(tirageNombre(5,50))
+}
+
+
+function init() {
+    // Ce script va permettre d'écrire dans les champs nombreA et nombreB
+// et lire la réponse de l'utilisateur, lorsqu'il cliquera le bouton.
+// v1.0 calcul des champs
+// v1.1 ajout du framework CSS MetreoUI
+// v1.2 ajout du bouton pour relancer le code
+    ecrireNombre("#nombreA", tirageNombre(1,50))
+    ecrireNombre("#nombreB", tirageNombre(1,50))
+    document.querySelector("#reponse").value = ""
+    document.querySelector("#reponse").style.backgroundColor = "white"
+    }
+    // Lancement d'une séquence de calcul
+    init()
+    
+
+function repondre(){
+    // cette fonction lit les champs et vérifie la bonne ou mauvaise réponse
+    nbreCoupJoue++
+    let resultatJuste=lireNombre("#nombreA")+lireNombre("#nombreB")
+    let resultatUser=lireNombre("#reponse")
+
+    // test d'égalité et modification de la couleur du champ réponse
+    if(resultatJuste==resultatUser){
+        document.querySelector("#reponse").style.backgroundColor= 
+        "#77FF00"
+        nbreBonneReponse++
+       }else{
+        document.querySelector("#reponse").style.backgroundColor =
+"#FFAA00"
+       }
+       reussite(nbreBonneReponse,nbreCoupJoue)
+}
+
+function reussite (bonne,coup){
+    // modifier la barre de réussite (MetroUI)
+    let progression = Metro.getPlugin("#progression","progress")
+    progression.buff(100)
+    progression.val(bonne/coup*100)
+    document.querySelector("#preussite").innerHTML="Réussite:"+bonne+"bonne(s) reponse(s) sur"+coup
+}
+
+let nbreBonneReponse = 0
+let nbreCoupJoue = 0
+init()
